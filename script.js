1addc0a19bd918159845a96f2b736878424bbcac
MazzGuille <mazzcode@gmail.com>
2022-03-22 11:10:28 -0300

    Finished!!!

--- script.js
+++ script.js
@@ -9,9 +9,31 @@ sr.reveal('.wrapper1',{
 const wrapper1 = document.querySelector('.wrapper1');
 const wrapper2 = document.querySelector('.wrapper2');
 const btn = document.querySelector('.btn1');
+const rating = document.querySelectorAll('.rating')
+const paragraphText = document.querySelector('.printed-result');
+let ratingR
 
-btn.addEventListener('click', function (){
-    wrapper1.classList.toggle('switch-1');
+rating.forEach((circle) =>{
+    circle.addEventListener('click', () => {
+      rating.forEach((ratingCircle) => {
+        if (circle === ratingCircle) {
+          ratingCircle.classList.add('rating-focus');
+        } else {
+          ratingCircle.classList.remove('rating-focus');
+        }
+      });
+      ratingR = Number(circle.textContent);
+    });
+  });
+
+
+
+btn.addEventListener('click', () =>{
+
+    if (!ratingR) {
+        alert('Please select a rating before proceeding');
+    } else{
+        wrapper1.classList.toggle('switch-1');
 
     sr.reveal('.wrapper2',{
         origin: 'bottom',
@@ -20,9 +42,14 @@ btn.addEventListener('click', function (){
         duration: 3500
     });
 
-    wrapper2.classList.toggle('switch-2');  
-    
-})
+    wrapper2.classList.toggle('switch-2');
+
+    paragraphText.textContent = `You selected ${ratingR} out of 5`;
+
+    }
+      
+
+});
 
 
 

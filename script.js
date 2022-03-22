e1dac00e98d0da8ffd6cc860c7fa1b2588dae4f0
MazzGuille <mazzcode@gmail.com>
2022-03-22 01:29:43 -0300

    added animations, missing star carry on

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.wrapper1',{
+    distance: '0px',
+    opacity: 0,
+    duration: 3500
+});
+
 const wrapper1 = document.querySelector('.wrapper1');
 const wrapper2 = document.querySelector('.wrapper2');
 const btn = document.querySelector('.btn1');
 
 btn.addEventListener('click', function (){
     wrapper1.classList.toggle('switch-1');
-    wrapper2.classList.toggle('switch-2');   
+
+    sr.reveal('.wrapper2',{
+        origin: 'bottom',
+        distance: '-100px',
+        opacity: 0,
+        duration: 3500
+    });
+
+    wrapper2.classList.toggle('switch-2');  
+    
 })
 
 
